0833bfbabe78aff236de993ab237166759dc0a72
Shamika Earle <e.shamika@yahoo.com>
2021-07-09 17:55:52 -0400

    feat: Added Logo and background image

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import logo from '../Assets/SE_logo.png';
+
+
+function Logo () {
+    return (
+        <a href='/'>
+            <img src={logo} className="App-logo"  alt="logo" />
+        </a>
+    )
+}
+
+export default Logo;
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,11 @@
 import './NavBar.css';
 import { Link } from 'react-router-dom';
+import logo from '../../Components/Assets/SE_logo.png'
 
 function NavBar() {
     return(
         <div className="navBar">
-            < Link to='/'>Logo</Link>
+            < Link to='/'>Logo<img src={logo}/> </Link>
             < Link to='/'>Landing</Link>
             < Link to='/about'>About</Link>
             < Link to='/projects'>Projects</Link>
