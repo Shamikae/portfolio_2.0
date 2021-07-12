2bbac6916a8dfac2c93e84c2a24c2601864786de
Shamika Earle <e.shamika@yahoo.com>
2021-07-12 16:11:31 -0400

    feat: Style LandingPage

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,8 +1,11 @@
+import './LandingPage.css';
+
 function LandingPage(){
     return (
         <div name='Landingpage' className='landingPage'>
             <h1>Shamika Earle</h1>
-            <p>Software Engineer & Developer based in New York, NY. Experienced in designing & developing.</p>
+            <h2>Hi, im Shamika Earle</h2>
+            <p id="land">Software Engineer & Developer based in New York, NY. Experienced in designing & developing.</p>
         </div>
     )
 }
