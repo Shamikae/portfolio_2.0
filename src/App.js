bb998b52304d68564787c89f6f2c63484b8da98b
shamikae <e.shamika@yahoo.com>
2021-10-05 02:52:07 -0400

    New Nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from  'react-router-dom';
 import SideNav from './Components/SideNav/SideNav';
 import About from './Components/About/About';
@@ -6,36 +7,51 @@ import LandingPage from './Components/LandingPage/LandingPage';
 import Projects from './Components/Projects/Projects';
 import Contact from './Components/Contact/Contact';
 import Resume from './Components/Resume/Resume';
+import {GiHamburgerMenu} from 'react-icons/gi'
 // import Scrollbar from 'smooth-scrollbar';
 
 // Scrollbar.init(document.querySelector('#my-scrollbar'));
-function openNav() {
-    return(
-    document.getElementById("mySidenav").style.width = "250px")
-  }
+// function openNav() {
+//     return(
+//     document.getElementById("mySidenav").style.width = "250px")
+//   }
 function App() {
+    const [ showNav, setShowNav ] = useState (false)
     return (
-        <div className="App">
-                        <span style={{fontSize:'30px', cursor:'pointer'}} onclick="openNav()">&#9776; open</span>
-            <SideNav/>
-            {/* <Router>
-                <Switch>
-                    <Route exact path="/" component={LandingPage} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/projects" component={Projects} />
-                    <Route exact path="/resume" component={Resume} />
-                    <Route exact path="/contact" component={Contact} />
-                </Switch>
-            </Router> */}
-            <LandingPage/>
-            <About/>
-            <Projects/>
-            <Resume/>
-            <Contact/>
-            
-        </div> 
+        // // <div className="App">
+        // //                 <span style={{fontSize:'30px', cursor:'pointer'}} onclick="openNav()">&#9776; open</span>
+        // //     <SideNav/>
+        //     {/* <Router>
+        //         <Switch>
+        //             <Route exact path="/" component={LandingPage} />
+        //             <Route exact path="/about" component={About} />
+        //             <Route exact path="/projects" component={Projects} />
+        //             <Route exact path="/resume" component={Resume} />
+        //             <Route exact path="/contact" component={Contact} />
+        //         </Switch>
+        //     </Router> */}//
+        // //     
         
-    )
+        
+            <Router>
+        <header>
+            <GiHamburgerMenu onClick={() => setShowNav(!showNav)}/>
+        </header>
+        <SideNav show={showNav}/>
+            <div className='main'>
+                <Route path="/" exact={true} component={LandingPage}/>
+                <Route path="/About" exact={true} component={About}/>
+                <Route path="/Projects" exact={true} component={Projects}/>
+                <Route path="/Contact" exact={true} component={Contact}/>
+                <Route path="/Resume" exact={true} component={Resume}/>
+                
+            </div>
+        </Router>
+
+            
+        
+        
+    );
 }
 
 export default App;
--- src/Components/SideNav/SideNav.js
+++ src/Components/SideNav/SideNav.js
@@ -1,26 +1,42 @@
 import './SideNav.css';
-import { Link } from 'react-scroll';
+// import { Link } from 'react-scroll';
+import { Link } from 'react-router-dom'
 import logo from '../Assets/SE_logo.png';
 // import Logo from '../Logo/Logo';
 
-  function closeNav() {
-      return(
-    document.getElementById("mySidenav").style.width = "0")
-  }
-function SideNav() {
-    return (
-        <div id='mySidenav' className="SideNav">
+//   function closeNav() {
+//       return(
+//     document.getElementById("mySidenav").style.width = "0")
+//   }
+// function SideNav() {
+//     return (
+//         <div id='mySidenav' className="SideNav">
             
 
-            < Link to="SideNav" class="closebtn" onclick="closeNav()">&times;</Link>
-            < Link to='landing' smooth={true}><img src={logo} alt="logo" className="App-logo" /></Link>
-            < Link to='landing' smooth={true}>Landing</Link>
-            < Link to='about' smooth={true}>About</Link>
-            < Link to='projects' smooth={true}>Projects</Link>
-            < Link to='resume' smooth={true}>Resume</Link>
-            < Link to='contact' smooth={true}>Contact</Link>
-            < Link  to='' smooth={true}>Light and Dark</Link>
+//             < Link to="SideNav" class="closebtn" onclick="closeNav()">&times;</Link>
+//             < Link to='landing' smooth={true}><img src={logo} alt="logo" className="App-logo" /></Link>
+//             < Link to='landing' smooth={true}>Landing</Link>
+//             < Link to='about' smooth={true}>About</Link>
+//             < Link to='projects' smooth={true}>Projects</Link>
+//             < Link to='resume' smooth={true}>Resume</Link>
+//             < Link to='contact' smooth={true}>Contact</Link>
+//             < Link  to='' smooth={true}>Light and Dark</Link>
             
+//         </div>
+//     )
+// }
+
+const SideNav = ({show}) => {
+    return (
+        <div className={show ? 'sidenav active' : 'sidenav'}>
+            <img src={logo} alt="Logo" className='logo' />
+                <ul>
+                <li>< Link to='/about' smooth={true}>About</Link></li>
+                <li>< Link to='/projects' smooth={true}>Projects</Link></li>
+                <li>< Link to='resume' smooth={true}>Resume</Link></li>
+                <li>< Link to='contact' smooth={true}>Contact</Link></li>
+                <li>< Link to='' smooth={true}>Light and Dark</Link></li>
+                </ul>
         </div>
     )
 }
